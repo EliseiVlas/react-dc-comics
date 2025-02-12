ceff3bc630f10a9dd62ad0508c4273370cfb1e9f
Elisei <eliseivlas.002@gmail.com>
2025-02-12 14:43:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header"
+import Main from "./components/main"
+import Footer from "./components/footer"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <Header />
+     <Main />
+     <Footer />
+    </>
+  )
+}
+
+export default App
