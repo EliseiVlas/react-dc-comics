8e8a86de8923a3b3fb932faad54c924e24f8f6f3
Elisei <eliseivlas.002@gmail.com>
2025-02-12 18:08:11 +0100

    consegna esercizio

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,73 @@
 export default function Footer() {
-    return (<main>Contenuto del footer</main>)
+    return (
+    <footer>
+        <div className="content-footer">
+            <div className="comics">
+                <div className="comics2">
+                    <div>
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>DC</h3>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li> Contact Us</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Magazine</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="dc-foto">
+                    <img src="./public/dc-logo-bg.png" alt="" />
+                </div>
+            </div>
+        </div>
+        <div className="banner-footer">
+            <div className="footer">
+                <div className="sign">
+                    <a href="#">SIGN-UP NOW!</a>
+                </div>
+                <div className="social">
+                    <a href="#">FOLLOW US</a>
+                    <img src="./public/footer-facebook.png" alt="" />
+                    <img src="./public/footer-twitter.png" alt="" />
+                    <img src="./public/footer-youtube.png" alt="" />
+                    <img src="./public/footer-pinterest.png" alt="" />
+                    <img src="./public/footer-periscope.png" alt="" />
+                </div>
+            </div>
+        </div>
+    </footer>
+    )
 }
