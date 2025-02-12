bd9854f2854c06443aa9e8a56ca12dede6aa79b7
Elisei <eliseivlas.002@gmail.com>
2025-02-12 16:50:57 +0100

    main completo

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -8,7 +8,26 @@ export default function Main() {
         </div>
         <div className="baner-main">
             <div>
-                di
+                <span>
+                    <img src="./public/buy-comics-digital-comics.png" alt="" />
+                    DIGITAL COMICS
+                </span>
+                <span>
+                    <img src="./public/buy-comics-merchandise.png" alt="" />
+                    DC MERCHANDISE
+                </span>
+                <span>
+                    <img src="./public/buy-comics-subscriptions.png" alt="" />
+                    SUBSCRIPTION
+                </span>
+                <span>
+                    <img src="./public/buy-comics-shop-locator.png" alt="" />
+                    COMIC SHOP LOCATOR
+                </span>
+                <span className="visa">
+                    <img src="./public/buy-dc-power-visa.svg" alt="" />
+                    DC POWER VISA
+                </span>
             </div>
         </div>
     </main>)
