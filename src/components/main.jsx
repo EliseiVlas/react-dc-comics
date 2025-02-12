c25631cd8581dbbfa622128208c6ffe8d70e25af
Elisei <eliseivlas.002@gmail.com>
2025-02-12 18:37:12 +0100

    CONSEGNA BONUS!!

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,3 +1,6 @@
+// RICHIAMO BANNER 
+import BanerMain from "./banner-main/main_secondary"
+
 export default function Main() {
     return (
     <main>
@@ -6,29 +9,6 @@ export default function Main() {
             <span>-- Contetnt goes here --</span>
             </article>
         </div>
-        <div className="baner-main">
-            <div>
-                <span>
-                    <img src="./public/buy-comics-digital-comics.png" alt="" />
-                    DIGITAL COMICS
-                </span>
-                <span>
-                    <img src="./public/buy-comics-merchandise.png" alt="" />
-                    DC MERCHANDISE
-                </span>
-                <span>
-                    <img src="./public/buy-comics-subscriptions.png" alt="" />
-                    SUBSCRIPTION
-                </span>
-                <span>
-                    <img src="./public/buy-comics-shop-locator.png" alt="" />
-                    COMIC SHOP LOCATOR
-                </span>
-                <span className="visa">
-                    <img src="./public/buy-dc-power-visa.svg" alt="" />
-                    DC POWER VISA
-                </span>
-            </div>
-        </div>
+             <BanerMain />
     </main>)
 }
